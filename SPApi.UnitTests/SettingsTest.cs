91f57fbf2c0d38c8b0f63bbf77f400ce05f3395f
Jason Wallace <wallace.jason@gmail.com>
2022-08-14 23:56:08 -0400

    Add settings unit test

---
+++ SPApi.UnitTests/SettingsTest.cs
@@ -0,0 +1,18 @@
+﻿namespace SPApi.UnitTests
+{
+    public class SettingsTest
+    {
+        [Fact]
+        public void ConstructorTest()
+        {
+            // Arrange / Act
+            var target = new Settings();
+
+            // Assert
+            target.RequireHttps.Should().BeTrue();
+            target.HelpKey.Should().BeNull();
+            target.EnableHelp.Should().BeFalse();
+            target.Endpoint.Should().Be("/api/data");
+        }
+    }
+}
